760a47fc2c730d261ee7aa0f5b4d0e330172708c
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-02-03 14:21:30 -0300

    Creation of the xcode project

---
+++ NanoChallenge7/NanoChallenge7/NanoChallenge7App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge7App.swift
+//  NanoChallenge7
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

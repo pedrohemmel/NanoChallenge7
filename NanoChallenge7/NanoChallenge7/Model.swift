36c8d1958d011cde135abbc7ed936e68cc7b5725
Levenetz <victorlevenetz@gmail.com>
2023-02-07 15:32:40 -0300

    Criação Arquivos arquitetura
    Criação arquivo model, movel view e iniciaçao da operação getAll da API

---
+++ NanoChallenge7/NanoChallenge7/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  NanoChallenge7
+//
+//  Created by Victor Levenetz Mariano on 07/02/23.
+//
+
+import Foundation
+
+struct artes: Codable, Hashable, Identifiable{
+    let id: Int
+    let nome: String
+}
+
---
+++ NanoChallenge7/NanoChallenge7/WebServices.swift
@@ -0,0 +1,35 @@
+//
+//  WebServices.swift
+//  NanoChallenge7
+//
+//  Created by Victor Levenetz Mariano on 07/02/23.
+//
+
+import Foundation
+
+class WebServices {
+        
+    func getAll(completion: @escaping ([artes]?) -> ()) {
+            
+            guard let url = URL(string: "lalla") else { return }
+            
+            URLSession.shared.dataTask(with: url) { data, resp, err in
+                
+                if err != nil {
+                    DispatchQueue.main.async {
+                        print(err!)
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                let posts = try? JSONDecoder().decode([artes].self, from: data!)
+                DispatchQueue.main.async {
+                    completion(posts)
+                }
+                
+            }.resume()
+            
+        }
+    
+}
